155c155a62b9493f7c364cbebc2b9dd341121b0f
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-12-30 00:02:03 -0700

    A bit of reformatting
    Just keeping in line with my own preferences, really. Since we're not going to ::continue:: from multiple places, I've put it back to a nested statement, I find that more readable for Lua. I've also moved bg_lut to just automatically generate, since we can depend on deterministic order.

--- data.lua
+++ data.lua
@@ -1,24 +1,33 @@
 local lut = {
-    White   = {255,255,255},
-    Gray    = {125,125,125},
-    Black   = {25,25,25},
-    Red     = {255,0,0},
-    Orange  = {255,100,0},
-    Yellow  = {255,225,0},
-    Lime    = {150,255,0},
-    Green   = {0,255,0},
-    Cyan    = {0,255,225},
-    Blue    = {0,0,255},
-    Purple  = {150,0,255},
-    Magenta = {255,0,255},
-    Pink    = {255,100,255}
+    White   = {255, 255, 255},
+    Gray    = {125, 125, 125},
+    Black   = {25, 25, 25},
+    Red     = {255, 0, 0},
+    Orange  = {255, 100, 0},
+    Yellow  = {255, 225, 0},
+    Lime    = {150, 255, 0},
+    Green   = {0, 255, 0},
+    Cyan    = {0, 255, 225},
+    Blue    = {0, 0, 255},
+    Purple  = {150, 0, 255},
+    Magenta = {255, 0, 255},
+    Pink    = {255, 100, 255}
 }
+
+-- Spritesheet position for the circuit value background on the pole tooltip
 local bg_lut = {
-    White=0,   Gray=1,     Black=2, Red=3,  Orange=4,
-    Yellow=5,  Lime=6,     Green=7, Cyan=8, Blue=9,
-    Purple=10, Magenta=11, Pink=12
+    -- White = {0, 0}
+    -- Gray = {36, 0}
+    -- etc
 }
-local wire_targets = {"red", "green", "copper"}
+
+-- In Factorio this order is deterministic, in normal Lua it is not
+local spritesheet_pos = 0
+for color in pairs(lut) do
+    bg_lut[color] = {spritesheet_pos, 0}
+    spritesheet_pos = spritesheet_pos + 36
+end
+
 local wire_graphics = {
     sprite    = "__CustomCircuitColors__/graphics/sprites/generic-wire.png",
     sprite_hr = "__CustomCircuitColors__/graphics/sprites/hr-generic-wire.png",
@@ -26,31 +35,41 @@ local wire_graphics = {
     connector = "__CustomCircuitColors__/graphics/icons/generic-wire-connectors.png",
     slot_lut  = "__CustomCircuitColors__/graphics/color_luts/circuit-network-content-slot-lut.png"
 }
-for _, _target in pairs(wire_targets) do
-    local wire_setting = settings.startup["cc-" .. _target .. "-color"].value
-    if wire_setting == "Default" then goto continue end
-    local wire_tint = lut[wire_setting]
-    local lut_pos = {bg_lut[wire_setting] * 36, 0}
-    local target = data.raw["utility-sprites"].default[_target .. "_wire"]
-    target.filename = wire_graphics.sprite
-    target.tint = wire_tint
-    target.hr_version.filename = wire_graphics.sprite_hr
-    target.hr_version.tint = wire_tint
-    if _target ~= "copper" then
-        local wire_icon = data.raw.item[_target .. "-wire"]
-        wire_icon.icon = nil
-        wire_icon.icon_size = 64
-        wire_icon.icons = {
-            {icon = wire_graphics.overlay, tint = wire_tint},
-            {icon = wire_graphics.connector}
-        }
-        wire_icon.localised_name = {
-            "item-name." .. _target .. "-wire",
-            {"color." .. wire_setting:lower()}
-        }
-        content_slot = data.raw["gui-style"].default[_target .. "_circuit_network_content_slot"]
-        content_slot.default_graphical_set.filename = wire_graphics.slot_lut
-        content_slot.default_graphical_set.position = lut_pos
+
+for _, wire_type in pairs({"red", "green", "copper"}) do
+    local wire_setting = settings.startup["cc-" .. wire_type .. "-color"].value
+
+    if wire_setting ~= "Default" then
+        -- Modify wire sprite
+        local wire_tint = lut[wire_setting]
+        local wire_sprite = data.raw["utility-sprites"].default[wire_type .. "_wire"]
+        wire_sprite.filename = wire_graphics.sprite
+        wire_sprite.tint = wire_tint
+        wire_sprite.hr_version.filename = wire_graphics.sprite_hr
+        wire_sprite.hr_version.tint = wire_tint
+
+        if wire_type ~= "copper" then
+            -- Modify red/green wire items
+            local wire_item = data.raw.item[wire_type .. "-wire"]
+            wire_item.localised_name = {
+                "item-name." .. wire_type .. "-wire",
+                {"color." .. wire_setting:lower()}
+            }
+            wire_item.icon = nil
+            wire_item.icon_size = 64
+            wire_item.icons = {
+                {
+                    icon = wire_graphics.overlay,
+                    tint = wire_tint
+                },
+                {
+                    icon = wire_graphics.connector
+                }
+            }
+            -- Circuit value background on tooltip
+            local content_slot = data.raw["gui-style"].default[wire_type .. "_circuit_network_content_slot"]
+            content_slot.default_graphical_set.filename = wire_graphics.slot_lut
+            content_slot.default_graphical_set.position = bg_lut[wire_setting]
+        end
     end
-    ::continue::
 end
